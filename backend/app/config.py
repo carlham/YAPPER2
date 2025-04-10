174b83588ad32038da38704cd34683395b068336
carlham <carlkh@stud.ntnu.no>
2025-04-10 14:33:22 +0200

    Backend should be deployable

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,4 @@
-DATABASE_URL = "postgresql://postgres:2345@localhost/Yapper" # replace with own database URL for development
+DATABASE_URL = "postgresql://yapperdb_user:GKwNgaw5YEyyYPSCCG3hVIayOmr90vvp@dpg-cvrqj7a4d50c73d3rimg-a.frankfurt-postgres.render.com:5432/yapperdb"
 #DATABASE_URL = "postgresql://postgres:jacob123@localhost:5432/YapperDB"
-SECRET_KEY = "your_secret_key"  #We should have something stronger later but like idc
-ALGORITHM = "HS256" #useless? idk
+SECRET_KEY = "your_secret_key"  
+ALGORITHM = "HS256" 
