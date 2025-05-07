7659d29d0032a115cd56d84964c4bd44535a98b6
Jakubo0451 <jakubo@stud.ntnu.no>
2025-05-07 14:45:50 +0200

    funny database ready

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,6 @@
 #DATABASE_URL = "postgresql://yapperdb_user:GKwNgaw5YEyyYPSCCG3hVIayOmr90vvp@dpg-cvrqj7a4d50c73d3rimg-a.frankfurt-postgres.render.com:5432/yapperdb"
 #DATABASE_URL = "postgresql://postgres:jacob123@localhost:5432/YapperDB"
 SECRET_KEY = "your_secret_key"  
-ALGORITHM = "HS256" 
-
-DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/YapperDB" # replace with own database URL for development
+ALGORITHM = "HS256"
+DATABASE_URL = "postgresql://admin:TZwcOLyfnM@2.56.164.114:5432/yapper" # replace with own database URL for development
 #DATABASE_URL = "postgresql://postgres:jacob123@localhost:5432/YapperDB"
